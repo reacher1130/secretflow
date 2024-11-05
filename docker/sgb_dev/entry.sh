3a609b2fb6283f3bc23fa82f72e7e45b030699c9
reacher1130 <1021902982@qq.com>
2024-11-05 19:25:44 +0800

    dddd

--- docker/sgb_dev/entry.sh
+++ docker/sgb_dev/entry.sh
@@ -21,9 +21,11 @@ cd src_copied
 
 
 source ~/.bashrc
-conda create -n build python=3.10 -y
 conda init
+conda create -n build python=3.10 -y
+
 conda activate build
+
 
 python3 setup.py bdist_wheel
 
